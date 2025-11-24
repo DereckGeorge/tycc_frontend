d38965328ba2b3056e8f841dbd5cd64b9901d1e5
DereckGeorge <eridericgeorge@gmail.com>
2025-11-24 16:48:19 +0300

    Remove partners section

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import { FeaturedPrograms } from "@/components/featured-programs"
 import { StatsSection } from "@/components/stats-section"
 import { TestimonialsSection } from "@/components/testimonials-section"
 import { NewsSection } from "@/components/news-section"
-import { PartnersSection } from "@/components/partners-section"
 
 export default function HomePage() {
   return (
@@ -13,7 +12,6 @@ export default function HomePage() {
       <StatsSection />
       <TestimonialsSection />
       <NewsSection />
-      <PartnersSection />
     </main>
   )
 }
